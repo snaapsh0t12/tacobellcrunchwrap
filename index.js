7d52abeb6e3474780cca23656d09d055cbf097e9
Aiden Lee <aidenjlee9478@gmail.com>
2026-09-10 18:09:29 -0500

    mmmm crunchy

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
 function yummyCrunchwrap() {
-    let sound = new Audio("./yummy.mp3");
+    let choice = Math.floor(Math.random() * 2);
+    console.log(choice);
+    var sound;
+    if (choice == 0) {
+        sound = new Audio("./yummy.mp3");
+    } else {
+        sound = new Audio("./crunch.mp3");
+    }
+    // let sound = new Audio("./yummy.mp3");
     console.log("yummy!");
     let playbackrate = Math.random() * (2.0 - 0.5);
     console.log(playbackrate);
+
     sound.playbackRate = playbackrate;
     // sound.playbackRate = 0.3;
     sound.play();
